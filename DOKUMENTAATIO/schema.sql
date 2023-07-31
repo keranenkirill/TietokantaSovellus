96c5732a8d1d0ec2710587b75b8c3a2f77a3d490
keranenkirill <keranen.kirill@gmail.com>
2023-07-31 19:39:13 +0300

    schema.sql: added otp_topics -table

--- DOKUMENTAATIO/schema.sql
+++ DOKUMENTAATIO/schema.sql
@@ -15,3 +15,13 @@ CREATE TABLE otp_users_info (
     age TEXT,
     hometown VARCHAR(100)
 );
+
+
+
+CREATE TABLE otp_topics (
+    id SERIAL PRIMARY KEY,
+    content VARCHAR(160) NOT NULL,
+    user_id INTEGER REFERENCES otp_users(id) ON DELETE CASCADE,
+    sent_at TIMESTAMP, 
+    topic VARCHAR(80) NOT NULL
+);
