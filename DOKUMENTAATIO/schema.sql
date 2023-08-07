1c3babdb68b1cb74ed368db451f1e1e4e4356fd6
keranenkirill <keranen.kirill@gmail.com>
2023-08-07 22:26:58 +0300

    new schema.sql

--- DOKUMENTAATIO/schema.sql
+++ DOKUMENTAATIO/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE otp_users
 (
-  user_id SERIAL PRIMARY KEY, 
+  user_id SERIAL PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
-  password TEXT NOT NULL, 
+  password TEXT NOT NULL,
   admin BOOL NOT NULL DEFAULT FALSE,
-  firstname VARCHAR(50) NOT NULL, 
-  lastname VARCHAR(50) NOT NULL, 
+  firstname VARCHAR(50) NOT NULL,
+  lastname VARCHAR(50) NOT NULL,
   hometown VARCHAR(75),
-  age TEXT NOT NULL 
+  age INT NOT NULL
 );
 
 CREATE TABLE otp_topics
@@ -27,6 +27,18 @@ CREATE TABLE otp_comments
   sent_at TIMESTAMP NOT NULL,
   topic_id INT NOT NULL,
   visible BOOL DEFAULT TRUE,
+  likes INT DEFAULT 0,
   FOREIGN KEY (user_id) REFERENCES otp_users(user_id),
   FOREIGN KEY (topic_id) REFERENCES otp_topics(topic_id)
 );
+
+CREATE TABLE otp_user_reactions
+(
+  user_id INT,
+  comment_id INT,
+  reaction_type VARCHAR(10), 
+  PRIMARY KEY (user_id, comment_id),
+  FOREIGN KEY (user_id) REFERENCES otp_users(user_id),
+  FOREIGN KEY (comment_id) REFERENCES otp_comments(comment_id)
+);
+
