2cb5eb3963dfc7c981f5f2050f9b8c810eedb45f
keranenkirill <keranen.kirill@gmail.com>
2023-08-07 12:38:54 +0300

    new database config

--- DOKUMENTAATIO/schema.sql
+++ DOKUMENTAATIO/schema.sql
@@ -1,27 +1,32 @@
-CREATE TABLE otp_users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    password TEXT NOT NULL,
-    admin BOOLEAN NOT NULL DEFAULT FALSE
+CREATE TABLE otp_users
+(
+  user_id SERIAL PRIMARY KEY, 
+  username VARCHAR(50) NOT NULL,
+  password TEXT NOT NULL, 
+  admin BOOL NOT NULL DEFAULT FALSE,
+  firstname VARCHAR(50) NOT NULL, 
+  lastname VARCHAR(50) NOT NULL, 
+  hometown VARCHAR(75),
+  age TEXT NOT NULL 
 );
 
-
-
-CREATE TABLE otp_users_info (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    firstname VARCHAR(50) NOT NULL,
-    lastname VARCHAR(50) NOT NULL,
-    age TEXT,
-    hometown VARCHAR(100)
+CREATE TABLE otp_topics
+(
+  topic_id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  topic_content VARCHAR(100) NOT NULL,
+  postable BOOL,
+  FOREIGN KEY (user_id) REFERENCES otp_users(user_id)
 );
 
-
-
-CREATE TABLE otp_topics (
-    id SERIAL PRIMARY KEY,
-    content VARCHAR(160) NOT NULL,
-    user_id INTEGER REFERENCES otp_users(id) ON DELETE CASCADE,
-    sent_at TIMESTAMP, 
-    topic VARCHAR(80) NOT NULL
+CREATE TABLE otp_comments
+(
+  comment_id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  comment_content VARCHAR(200) NOT NULL,
+  sent_at TIMESTAMP NOT NULL,
+  topic_id INT NOT NULL,
+  visible BOOL DEFAULT TRUE,
+  FOREIGN KEY (user_id) REFERENCES otp_users(user_id),
+  FOREIGN KEY (topic_id) REFERENCES otp_topics(topic_id)
 );
